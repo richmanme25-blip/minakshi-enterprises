4e6632ffe837f07756fc7a69cc8ba6bdf1cb8250
richmanme25-blip <richmanme25@gmail.com>
2026-07-20 06:27:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Minakshi Enterprises Website
+console.log("Welcome to Minakshi Enterprises");
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to Minakshi Enterprises!");
+});
